b802d79ca4d8a334c022b9c73465e3b76ae94d3a
Adrien Champion <adrien.champion@protonmail.com>
2024-08-06 15:10:42 +0200

    chore: turn tests into a library, rely on lake instead of manual driver/runner

---
+++ Cvc5Test/MkTerms1.lean
@@ -0,0 +1,58 @@
+import Test.Init
+
+namespace cvc5.Test
+
+def mkTerms1 : IO Unit := do
+  let tm ← TermManager.new
+
+  let boolKind := Kind.CONST_BOOLEAN
+
+  let (tru, fls) := (
+    tm.mkBoolean true,
+    tm.mkBoolean false,
+  )
+  assertEq #[tru.getKind, boolKind]
+  assertEq #[tru.getSort.toString, "Bool"]
+  assertEq #[fls.getKind, boolKind]
+  assertEq #[fls.getSort.toString, "Bool"]
+
+  let intKind := Kind.CONST_INTEGER
+
+  let (one, three, seven, eleven) := (
+    tm.mkInteger 1,
+    tm.mkInteger 3,
+    tm.mkInteger 7,
+    tm.mkInteger 11,
+  )
+  assertEq #[one.getKind, intKind]
+  assertEq #[one.getSort.toString, "Int"]
+  assertEq #[three.getKind, intKind]
+  assertEq #[three.getSort.toString, "Int"]
+  assertEq #[seven.getKind, intKind]
+  assertEq #[seven.getSort.toString, "Int"]
+  assertEq #[eleven.getKind, intKind]
+  assertEq #[eleven.getSort.toString, "Int"]
+
+  let ite1 :=
+    tm.mkTerm Kind.ITE #[fls, three, seven]
+  assertEq #[ite1.getKind, Kind.ITE]
+  assertEq #[ite1.getSort.toString, "Int"]
+
+  let eq1 :=
+    tm.mkTerm Kind.EQUAL #[ite1, eleven]
+  assertEq #[eq1.getKind, Kind.EQUAL]
+  assertEq #[eq1.getSort.toString, "Bool"]
+
+  let eq1' :=
+    tm.mkTerm Kind.EQUAL #[ite1, eleven, one]
+  assertEq #[eq1'.getKind, Kind.AND]
+  assertEq #[eq1'.getSort.toString, "Bool"]
+
+  let ite2 :=
+    tm.mkTerm Kind.ITE #[tru, eq1, fls]
+  assertEq #[ite2.getKind, Kind.ITE]
+  assertEq #[ite2.getSort.toString, "Bool"]
+
+/-- info: -/
+#guard_msgs in
+  #eval mkTerms1
---
+++ Cvc5Test/Solver1Parse.lean
@@ -0,0 +1,68 @@
+import Test.Init
+
+namespace cvc5.Test
+
+def solver1Parse : IO Unit := do
+  let tm ← TermManager.new
+
+  let query :=
+    "
+(set-logic QF_LIA)
+
+(declare-fun n1 () Int)
+(declare-fun n2 () Int)
+
+(declare-fun b () Bool)
+
+(assert (ite b (= n1 n2) (not (= n1 n2))))
+(assert (= n1 n2))
+    ".smtParseAnd Solver.checkSat?
+
+  match ← Solver.run! tm query with
+  | none =>
+    panic! "got a timeout"
+  | some false =>
+    panic! "unexpected `unsat` result"
+  | some true =>
+    println! "confirmed `sat` result"
+
+
+
+  let query : SolverM (Array Proof) :=
+    "
+(set-option :produce-proofs true)
+
+(set-logic QF_LIA)
+
+(declare-fun n1 () Int)
+(declare-fun n2 () Int)
+
+(declare-fun b () Bool)
+
+(assert (=> b (= n1 n2)))
+(assert (=> (not b) (not (= n1 n2))))
+(assert (= n1 n2))
+(assert (not b))
+    ".smtParseAnd
+    do
+      match ← Solver.checkSat? with
+      | none => panic! "got a timeout"
+      | some false => println! "confirmed `unsat` result"
+      | some true => panic! "unexpected `sat` result"
+
+      Solver.getProof
+
+  let proofs ← Solver.run! tm query
+
+  println! "proof:"
+  for p in proofs do
+    println! "- {p.getResult}"
+
+/-- info:
+confirmed `sat` result
+confirmed `unsat` result
+proof:
+- (let ((_let_1 (not b))) (let ((_let_2 (= n1 n2))) (not (and (=> b _let_2) (=> _let_1 (not _let_2)) _let_2 _let_1))))
+-/
+#guard_msgs in
+  #eval solver1Parse
