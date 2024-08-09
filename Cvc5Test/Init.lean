2ef18c902a18eac976cc882d865bce30fcb1f8a9
Adrien Champion <adrien.champion@protonmail.com>
2024-08-09 09:37:03 +0200

    chore(test): throw server error on assertion failure

--- Cvc5Test/Init.lean
+++ Cvc5Test/Init.lean
@@ -1,16 +1,28 @@
+import Lean.Server.Utils
+
 import cvc5
 
 namespace cvc5
 
-def Test.assertEq [ToString α] [BEq α] (lft rgt : α) (hint := "") : IO Unit := do
+namespace Test
+
+def fail (s : String) : IO Unit :=
+  IO.throwServerError s
+
+
+def assertEq [ToString α] [BEq α] (lft rgt : α) (hint := "") : IO Unit := do
   if lft != rgt then
     let pref := if hint.isEmpty then "" else s!"[{hint}] "
     IO.eprintln s!"{pref}comparison failed: `{lft}` is different from `{rgt}`"
+    fail "assertion failed"
 
-def Test.assertNe [ToString α] [BEq α] (lft rgt : α) (hint := "") : IO Unit := do
+def assertNe [ToString α] [BEq α] (lft rgt : α) (hint := "") : IO Unit := do
   if lft == rgt then
     let pref := if hint.isEmpty then "" else s!"[{hint}] "
     IO.eprintln s!"{pref}comparison failed: `{lft}` is the same as `{rgt}`"
+    fail "assertion failed"
+
+end Test
 
 namespace Solver
 
