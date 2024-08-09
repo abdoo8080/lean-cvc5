5be7ebcbbf9011defd27d2892592e6f955df6f9d
Adrien Champion <adrien.champion@protonmail.com>
2024-08-09 17:47:29 +0200

    chore: remove `TermManager.mkConst` and irrelevant cvc5 api test

--- cvc5/Solver.lean
+++ cvc5/Solver.lean
@@ -450,17 +450,6 @@ Create operators with `mkOp`.
 @[extern "termManager_mkTermOfOp"]
 opaque mkTermOfOp : TermManager → (op : Op) → (children : Array Term := #[]) → Term
 
-/-- Create a free constant.
-
-Note that the returned term is always fresh, even if the same arguments were provided on a previous
-call to `mkConst`.
-
-- `sort` The sort of the constant.
-- `symbol` The name of the constant.
--/
-@[extern "termManager_mkConst"]
-opaque mkConst : TermManager → (sort : cvc5.Sort) → (symbol : String) → Term
-
 end TermManager
 
 namespace Solver
