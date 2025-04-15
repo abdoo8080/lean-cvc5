5bba0acb10237c305e490bf88e9482c19acc1a8e
Adrien Champion <adrien.champion@protonmail.com>
2025-04-15 16:07:00 +0200

    chore: add copyright in sort test file

--- cvc5Test/Unit/ApiSort.lean
+++ cvc5Test/Unit/ApiSort.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2023-2024 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Abdalrhman Mohamed, Adrien Champion
+-/
 import cvc5Test.Init
 
 /-! # Black box testing of the guards of the Lean API functions
