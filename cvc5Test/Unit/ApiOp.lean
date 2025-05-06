bf3efb158ada95055dede94e0a3c150cd726782c
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2025-05-05 17:40:36 -0700

    Update Lean toolchain to v4.18.0.

--- cvc5Test/Unit/ApiOp.lean
+++ cvc5Test/Unit/ApiOp.lean
@@ -159,7 +159,7 @@ test! tm => do
   let indices := #[0, 3, 2, 0, 1, 2];
   let tupleProject ← tm.mkOp Kind.TUPLE_PROJECT indices;
   for idx in [0 : indices.size] do
-    check tupleProject idx (indices.get! idx)
+    check tupleProject idx indices[idx]!
 
 /-
 Not sure what to do for the end of the test below. Original test is
